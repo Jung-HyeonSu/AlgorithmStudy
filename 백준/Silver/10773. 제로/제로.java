2171e65afd590781a67cae63499f75ea38910d70
Hyeonsu <92723729+Jung-HyeonSu@users.noreply.github.com>
2023-10-25 23:41:42 +0900

    [Silver IV] Title: 제로, Time: 220 ms, Memory: 22172 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        int top = -1;
+
+        for(int i=0;i<n;i++){
+            int number = Integer.parseInt(br.readLine());
+
+            if(number == 0){
+                arr[top] = 0;
+                top--;
+            }
+            else{
+                top++;
+                arr[top] = number;
+            }
+        }
+
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+    }
+}
