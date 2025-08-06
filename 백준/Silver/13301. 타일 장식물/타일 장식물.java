b6a6cdcd3b622e1eaae2b87509fdd9a1324ef90f
Hyeonsu <92723729+Jung-HyeonSu@users.noreply.github.com>
2025-08-06 14:25:29 +0900

    [Silver V] Title: 타일 장식물, Time: 108 ms, Memory: 14272 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13301. 타일 장식물/타일 장식물.java
+++ 백준/Silver/13301. 타일 장식물/타일 장식물.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        long[] dp = new long[80];
+        long[] dp = new long[81];
         dp[0] = 1;
         dp[1] = 1;
         for(int i=2; i<N+1; i++){
